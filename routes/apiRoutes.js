579b7a24626b350bedd5f00eada350f50f6a5942
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-10-03 23:29:59 -0500

    "routing issues"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,21 +3,15 @@ const path = require('path');
 const fs = require('fs')
 const router = require('express').Router()
 
-
-
-
-
-
 // routing
 
 
-
-router.get('/notes', (req, res) => {
+router.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
 });
 
 
-router.post('/notes', (req, res) => {
+router.post('/api/notes', (req, res) => {
     let db = fs.readFileSync('db/db.json');
     db = JSON.parse(db);
     res.json(db);
@@ -26,7 +20,7 @@ router.post('/notes', (req, res) => {
         title: req.body.title,
         text: req.body.text,
 
-
+        id: uniqid(),
     };
 
     db.push(userNote);
@@ -36,7 +30,7 @@ router.post('/notes', (req, res) => {
 });
 
 
-router.delete('/notes/:id', (req, res) => {
+router.delete('/api/notes/:id', (req, res) => {
 
     let db = JSON.parse(fs.readFileSync('db/db.json'))
 
@@ -46,6 +40,3 @@ router.delete('/notes/:id', (req, res) => {
     res.json(deleteNotes);
 
 })
-
-module.exports = router;
-
