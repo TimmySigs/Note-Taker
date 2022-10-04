93a11fd02cc142999203c723b88b6a76aa90ac51
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-10-04 00:45:02 -0500

    "found and fixed route bug"

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 // routing
 
-router.get('/notes.', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
