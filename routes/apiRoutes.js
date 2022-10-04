356613209dd865131661ed14aa44e84a3da2abbf
TimmySigs <108436098+TimmySigs@users.noreply.github.com>
2022-10-04 00:13:08 -0500

    "routes"

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,44 @@
 // dependencies 
 const path = require('path');
 const fs = require('fs')
-const router = require('express').Router()
+
 
 // routing
+module.exports = (app) => {
 
 
-router.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../db/db.json'));
-});
+    app.get('/api/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../db/db.json'));
+    });
 
 
-router.post('/api/notes', (req, res) => {
-    let db = fs.readFileSync('db/db.json');
-    db = JSON.parse(db);
-    res.json(db);
+    app.post('/api/notes', (req, res) => {
+        let db = fs.readFileSync('db/db.json');
+        db = JSON.parse(db);
+        res.json(db);
 
-    let userNote = {
-        title: req.body.title,
-        text: req.body.text,
+        let userNote = {
+            title: req.body.title,
+            text: req.body.text,
 
-        id: uniqid(),
-    };
+        };
 
-    db.push(userNote);
-    fs.writeFileSync('db/db.json', JSON.stringify(db));
-    res.json(db);
+        db.push(userNote);
+        fs.writeFileSync('db/db.json', JSON.stringify(db));
+        res.json(db);
 
-});
+    });
 
 
-router.delete('/api/notes/:id', (req, res) => {
+    app.delete('/api/notes/:id', (req, res) => {
 
-    let db = JSON.parse(fs.readFileSync('db/db.json'))
+        let db = JSON.parse(fs.readFileSync('db/db.json'))
 
-    let deleteNotes = db.filter(item => item.id !== req.params.id);
+        let deleteNotes = db.filter(item => item.id !== req.params.id);
 
-    fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
-    res.json(deleteNotes);
+        fs.writeFileSync('db/db.json', JSON.stringify(deleteNotes));
+        res.json(deleteNotes);
 
-})
+    })
+};
+
